202ea2349a0fc79f04482ba2606453dcbcfa1f6f
Samuel Lúcio Moreira de Farias <20222014040004@ifrn.local>
2023-09-14 10:09:20 -0300

    SolucaoDIPEQ

---
+++ src/main/java/com/solucao/cadempresas/CadempresasApplication.java
@@ -0,0 +1,13 @@
+package com.solucao.cadempresas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadempresasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadempresasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solucao/cadempresas/CadempresasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solucao.cadempresas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadempresasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
